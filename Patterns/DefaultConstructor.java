814f7d7ec0585b35f391849354bfaa69be1f8f5f
Gagan-42 <gagan21212121.21.21@gmail.com>
2024-02-27 03:16:57 +0545

    initial commit

--- Patterns/DefaultConstructor.java
+++ Patterns/DefaultConstructor.java
@@ -3,7 +3,7 @@ package Patterns;
 public class DefaultConstructor {
 
     DefaultConstructor(){
-        System.out.println("HEllo World");
+        System.out.println("Hello World");
     }
     public static void main(String[] args) {
 DefaultConstructor rohit = new DefaultConstructor();
